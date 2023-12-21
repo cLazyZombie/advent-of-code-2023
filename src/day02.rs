70f2cd8e205dc24998ce6d125ecbb3971f7f550e
cLazyZombie <rusad96@gmail.com>
2023-12-21 14:45:00 +0900

    add more test to day02

--- src/day02.rs
+++ src/day02.rs
@@ -132,7 +132,7 @@ mod part1 {
         use super::*;
 
         #[test]
-        fn test_solve() {
+        fn test_solve_sample() {
             let input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
@@ -140,6 +140,12 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
 
             assert_eq!(solve(input), 8);
+        }
+
+        #[test]
+        fn test_solve() {
+            let input = include_str!("day02.txt");
+            assert_eq!(solve(input), 1931);
         }
     }
 }
@@ -189,6 +195,12 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
             assert_eq!(solve(input), 2286);
+        }
+
+        #[test]
+        fn test_solve() {
+            let input = include_str!("day02.txt");
+            assert_eq!(solve(input), 83105);
         }
     }
 }
